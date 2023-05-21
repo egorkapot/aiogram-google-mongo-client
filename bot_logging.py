ac48b7b177d6008f05ac6c9518e4b9faf02aa7a3
egorkapot <makaroffglobal@icloud.com>
2023-05-22 02:26:41 +0300

    add google auth | logging

---
+++ bot_logging.py
@@ -0,0 +1,13 @@
+import logging
+from telebot import TeleBot
+
+
+class BotLoggingHandler(logging.Handler):
+    def __init__(self, bot: TeleBot, chat_id: str):
+        super().__init__()
+        self.bot = bot
+        self.chat_id = chat_id
+
+    def emit(self, record: logging.LogRecord) -> None:
+        log_entry = self.format(record)
+        self.bot.send_message(self.chat_id, log_entry)
--- main.py
+++ main.py
@@ -5,9 +5,8 @@ from telebot import types
 from emails.emails import get_emails_list
 
 load_dotenv()
-
-
 bot_token = os.environ.get("BOT_TOKEN")
+author_chat_id = os.environ.get("AUTHOR_CHAT_ID")
 bot = telebot.TeleBot(bot_token)
 list_of_emails = get_emails_list()
 
@@ -15,20 +14,21 @@ list_of_emails = get_emails_list()
 @bot.message_handler(commands=["start"])
 def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    open_access = types.KeyboardButton("Открыть доступ")
-    link_to_table = types.KeyboardButton("Ссылка на рабочую таблицу")
-    link_to_guide = types.KeyboardButton("Ссылка на гайд")
-    markup.add(open_access, link_to_table, link_to_guide)
-    bot.send_message(message.from_user.id, "Выбери что тебе нужно", reply_markup=markup)
+    open_access = types.KeyboardButton("Open the access")
+    link_to_table = types.KeyboardButton("Working Table")
+    link_to_guide = types.KeyboardButton("Link to Guide")
+    markup.row(link_to_table, link_to_guide)
+    markup.row(open_access)
+    bot.send_message(message.from_user.id, "Choose what you need", reply_markup=markup)
 
 
 @bot.message_handler(content_types=["text"])
 def message_handling(message):
-    if message.text == "Открыть доступ":
+    if message.text == "Open the access":
         bot.send_message(message.from_user.id, "Not implemented yet")
-    elif message.text == "Ссылка на рабочую таблицу":
+    elif message.text == "Working Table":
         bot.send_message(message.from_user.id, os.environ.get("TABLE_LINK"))
-    elif message.text == "Ссылка на гайд":
+    elif message.text == "Link to Guide":
         bot.send_message(message.from_user.id, os.environ.get("GUIDE_LINK"))
 
 
