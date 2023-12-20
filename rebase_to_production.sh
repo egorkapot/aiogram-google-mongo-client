3825180c209751e1e5e64362a6a119a8ac7a187e
Egor Makarov <egor.makarov@predyktable.com>
2023-12-20 23:51:09 +0100

    another fix

--- rebase_to_production.sh
+++ rebase_to_production.sh
@@ -1,4 +1,7 @@
 #Script to add changes from main branch to production
+git checkout main
+git add .
+git commit -m "another fix"
 git checkout production
 git rebase main
 git push origin production --force
