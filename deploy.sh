a5d572a54177de34297e86995f41ef32aca21f87
Egor Makarov <egor.makarov@predyktable.com>
2023-12-21 21:20:05 +0100

    add tier

--- deploy.sh
+++ deploy.sh
@@ -20,8 +20,19 @@ else
     echo "Docker Compose is already installed."
 fi
 
+#Create a tier based on the first argument while running script
+TIER=$1
+if [ "$TIER" = "production" ] || [ "$TIER" = "development" ]; then
+    export $TIER
+  else
+    echo "Invalid environment specified"
+    exit 1
+fi
+
+
 echo "Creating .env file..."
 cat << EOF > .env
+TIER=$TIER
 PRODUCTION_BOT_TOKEN=$PRODUCTION_BOT_TOKEN
 DEVELOPMENT_BOT_TOKEN=$DEVELOPMENT_BOT_TOKEN
 AUTHOR_CHAT_ID=$AUTHOR_CHAT_ID
@@ -34,24 +45,5 @@ DUPLICHEKER_TOKEN=$DUPLICHEKER_TOKEN
 WEB_CONTENT_CHAT_ID=$WEB_CONTENT_CHAT_ID
 EOF
 
-echo "Printing environment variable values for debugging..."
-echo "PRODUCTION_BOT_TOKEN: $PRODUCTION_BOT_TOKEN"
-echo "DEVELOPMENT_BOT_TOKEN: $DEVELOPMENT_BOT_TOKEN"
-echo "AUTHOR_CHAT_ID: $AUTHOR_CHAT_ID"
-echo "GUIDE_LINK: $GUIDE_LINK"
-echo "WEB_CONTENT_TABLE_LINK: $WEB_CONTENT_TABLE_LINK"
-echo "WEB_AI_CONTENT_TABLE_LINK: $WEB_AI_CONTENT_TABLE_LINK"
-echo "SEO_CONTENT_TABLE_LINK: $SEO_CONTENT_TABLE_LINK"
-echo "BACKUP_TABLE_LINK: $BACKUP_TABLE_LINK"
-echo "DUPLICHEKER_TOKEN: $DUPLICHEKER_TOKEN"
-echo "WEB_CONTENT_CHAT_ID: $WEB_CONTENT_CHAT_ID"
-
-TIER=$1 #pass the tier as the first argument while running script
-if [ "$TIER" = "production" ] || [ "$TIER" = "development" ]; then
-    export $TIER
-  else
-    echo "Invalid environment specified"
-    exit 1
-fi
 
 docker-compose up --build -d
